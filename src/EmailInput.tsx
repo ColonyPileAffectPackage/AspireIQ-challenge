80fab24ce4d2cc3cdc450a55884ea96fc598851c
Keri Warr <keriwarrr+aspireiq@gmail.com>
2020-07-13 23:59:03 -0400

    Add basic address submissin behaviour

--- src/EmailInput.tsx
+++ src/EmailInput.tsx
@@ -1,13 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./EmailInput.css";
 
+const makeUniqueIdFactory = () => {
+  let counter = 0;
+
+  return () => {
+    counter += 1;
+    return counter;
+  };
+};
+
 export const EmailInput: React.FC = () => {
+  const getUniqueId = useRef(makeUniqueIdFactory()).current;
   const [inputText, setInputText] = useState("");
+  const [addresses, setAddresses] = useState<
+    Array<{ address: string; id: number }>
+  >([]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Tab" || e.key === "Enter") {
+      e.preventDefault();
+      setAddresses([...addresses, { address: inputText, id: getUniqueId() }]);
+      setInputText("");
+    }
+  };
+
   return (
-    <input
-      className="EmailInput"
-      onChange={e => setInputText(e.target.value)}
-      value={inputText}
-    />
+    <div className="EmailInput-container">
+      <div className="EmailInput-address-list">
+        {addresses.map(({ address, id }) => (
+          <span key={id}>{address}</span>
+        ))}
+      </div>
+      <input
+        type="text"
+        className="EmailInput-input"
+        onChange={e => setInputText(e.target.value)}
+        onKeyDown={handleKeyDown}
+        value={inputText}
+      />
+    </div>
   );
 };
