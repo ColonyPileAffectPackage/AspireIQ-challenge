9b67bd1967153eec5dc594e07dd89cce1cd75873
Keri Warr <keriwarrr+aspireiq@gmail.com>
2020-07-13 23:59:03 -0400

    Add a background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import figmaBackground from "./figma-background.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={figmaBackground} className="App-background" alt="background" />
     </div>
   );
 }
